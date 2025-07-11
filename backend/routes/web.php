d28d4eee6ee9734b0945a8b0323f985ad3725611
hafizalkariem <ahapiz95@gmail.com>
2025-07-12 00:18:50 +0700

    add dcokerfile again and again

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/health', function () {
     return response()->json([
         'status' => 'OK',
