999cabe2e8909e83d11211a38b375a652488d486
hafizalkariem <ahapiz95@gmail.com>
2025-07-12 00:31:59 +0700

    add dcokerfile again and again

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -5,7 +5,7 @@ chown -R www-data:www-data /app/storage /app/bootstrap/cache /app/database
 chmod -R 777 /app/storage /app/bootstrap/cache /app/database
 
 # Jalankan migrasi database dari awal dan seed data
-php artisan migrate
+# php artisan migrate
 
 # Jalankan Supervisor untuk Nginx dan PHP-FPM
 exec /usr/bin/supervisord -c /etc/supervisord.conf
